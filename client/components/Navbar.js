03b456b4e27ae92f5bb52634be89c7eeea3234d3
Onur <onurbelek@outlook.com>
2023-05-17 00:37:24 +0100

    made the cursor pointer in the logo

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -7,11 +7,11 @@ export default function Navbar() {
     <>
       <header className="w-full">
         <nav className="text-slate-100 flex justify-center bg-[#022f58]">
-          <div className="flex justify-center items-center">
-            <Link href="/" className="cursor-pointer">
+          <div className="flex justify-center items-center cursor-pointer">
+            <Link href="/">
               <Image src="/automata_logo_2.jpg" width={100} height={100} />
             </Link>
-            <Link href="/" className="cursor-pointer">
+            <Link href="/">
               <h1 className="text-4xl font-semibold">Automata</h1>
             </Link>
           </div>
